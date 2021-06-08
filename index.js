eba7dc8ed1e5341c35423a2f5896ca5fe9cf8fc4
Jarad Solomon <jaradsolomon@gmail.com>
2021-06-07 22:36:50 -0500

    commit tree 5708e4d5f891b0e55077ccdd24e1475b32f6da21

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded",addEvent)
+
+function addEvent(){
+    const para = document.getElementById("text");
+    para.innerHTML = "This is really cool!"
+}
